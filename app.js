a5bfe2d5ad1466ac96473e58f2d498a9180d6233
Altus Janse Van Rensburg <albertusbjvr@gmail.com>
2020-09-03 14:41:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+const port = process.env.PORT || 8080;
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+
+app.listen(port, function () {
+  console.log("Active on port " + port)
+});
+
+// ======== Models Import ========
+const Todo = require('./models/todo');
+
+// ======== Mongoose Connect ========
+
+mongoose.connect("mongodb://localhost/todo_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to DB!'))
+  .catch(error => console.log(error.message));
+
+// ====== Routes ========
+app.get("/", function (req, res) {
+  res.redirect("/todo");
+});
+// ======== Show all todos and create todos ========
+app.get("/todo", function (req, res) {
+  Todo.find({}, function (err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Yeet")
+      res.render('todos', { todos: todos })
+    }
+  })
+});
+// ======== Create a new Todo ========
+app.post("/todo", function (req, res) {
+  Todo.create(req.body.todo, function (err, newTodo) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect('/todo')
+      console.log(req.body.todo)
+    }
+  });
+});
+
+// ======= Update Todo =======
+app.put("/todo/update/:id", function (req, res) {
+  Todo.findById(req.params.id, function (err, todo) {
+    todo.done = !todo.done;
+    todo.save(function (err, updatedTodo) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/")
+      }
+    })
+  })
+});
+
+// ======== Delete todo =======
+app.delete("/todo/delete/all", function (req, res) {
+  Todo.deleteMany({}, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("DB Cleared");
+    res.redirect("/");
+  });
+});
+
+app.delete("/todo/delete/:id", function (req, res) {
+  Todo.findByIdAndRemove(req.params.id, req.body.todo, function (err, dTodo) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect('/')
+    }
+  })
+});
+
